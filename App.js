feaa4407c36add1d0db3c26c5cc1da953af24eca
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-02-06 18:09:37 +0100

    styled bottom navigation icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import RepositoriesScreen from "./screens/RepositoriesList";
 import ProfileScreen from "./screens/Profile";
 
@@ -9,7 +10,24 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Profile") {
+              iconName = focused ? "person-circle" : "person-circle-outline";
+            } else if (route.name === "Repos") {
+              iconName = focused ? "list-circle" : "list-circle-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "gray",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="Repos" component={RepositoriesScreen} />
       </Tab.Navigator>
